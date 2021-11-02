9421499278b2b88f60556a7bf45043a63e669826
Ladrillo <gabriel@cabrejas.net>
2021-11-02 18:53:42 +0100

    wip

--- data/seeds/add-fruits.js
+++ data/seeds/add-fruits.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  // TRUNCATE ALL existing entries
+  return knex('table_name').truncate() 
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([

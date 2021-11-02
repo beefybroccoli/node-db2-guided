1999b2447d838f8950325f6bc2baf24d02512442
Ladrillo <gabriel@cabrejas.net>
2021-11-02 17:46:28 +0100

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -5,3 +5,6 @@ const settings = require('../knexfile')
 
 const env = process.env.NODE_ENV || 'development'
 const settingsToUse = settings[env]
+const connection = knex(settingsToUse)
+
+module.exports = connection

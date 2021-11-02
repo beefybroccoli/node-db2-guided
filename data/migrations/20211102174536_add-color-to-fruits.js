1a875bd4970f677e20f8fb71e04405cb2897d21e
Ladrillo <gabriel@cabrejas.net>
2021-11-02 18:47:11 +0100

    wip

--- data/migrations/20211102174536_add-color-to-fruits.js
+++ data/migrations/20211102174536_add-color-to-fruits.js
@@ -2,8 +2,10 @@ exports.up = function(knex) {
   return knex.schema.table('fruits', table => {
     table.string('color', 128)
   })
-};
+}
 
 exports.down = function(knex) {
-
-};
+  return knex.schema.table('fruits', table => {
+    table.dropColumn('color')
+  })
+}

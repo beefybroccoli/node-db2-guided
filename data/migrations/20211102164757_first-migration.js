0f3c92e07947d8f879fe4928f544088e91a80afc
Ladrillo <gabriel@cabrejas.net>
2021-11-02 18:22:48 +0100

    wip

--- data/migrations/20211102164757_first-migration.js
+++ data/migrations/20211102164757_first-migration.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('fruits', table => {
     table.increments('fruit_id') // shortcut for 'id' column, primary key, auto inc integers
     table.text('fruit_name', 128) // this would work with other dbs too (not only sqlite)
+      .unique().notNullable()
     table.decimal('avg_weight')
     table.boolean('delicious')
   })

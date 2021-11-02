568e22a53898a72bb90a7187606803fbdc3c6fa2
Ladrillo <gabriel@cabrejas.net>
2021-11-02 17:33:27 +0100

    wip

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,10 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/produce.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      
     }
   },
   testing: {

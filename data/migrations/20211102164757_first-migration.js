ef020a7a64dabd7936910afbc3def9e662690be0
Ladrillo <gabriel@cabrejas.net>
2021-11-02 18:24:22 +0100

    wip

--- data/migrations/20211102164757_first-migration.js
+++ data/migrations/20211102164757_first-migration.js
@@ -1,13 +1,14 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('fruits', table => {
-    table.increments('fruit_id') // shortcut for 'id' column, primary key, auto inc integers
-    table.text('fruit_name', 128) // this would work with other dbs too (not only sqlite)
-      .unique().notNullable()
+    // shortcut for 'id' column, primary key, auto inc integers
+    table.increments('fruit_id')
+    // this would work with other dbs too (not only sqlite)
+    table.text('fruit_name', 128).unique().notNullable()
     table.decimal('avg_weight').notNullable()
     table.boolean('delicious')
   })
 }
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('fruits')
 }

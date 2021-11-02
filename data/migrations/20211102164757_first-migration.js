4c1ee327fe36ad99e622a95b41e5ad597ea85454
Ladrillo <gabriel@cabrejas.net>
2021-11-02 17:59:23 +0100

    wip

--- data/migrations/20211102164757_first-migration.js
+++ data/migrations/20211102164757_first-migration.js
@@ -1,9 +1,9 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('fruits', table => {
-
+    table.increments() // shortcut for 'id' column, primary key, auto inc integers
   })
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists()
+  await knex.schema.dropTableIfExists('fruits')
 };
